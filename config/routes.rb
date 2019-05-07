4d16a126e69d6c8edb17c8931d3bfdbbe7e4ce42
Raghu Betina <raghu@firstdraft.com>
2019-05-07 08:05:40 -0500

    Update routes to use match instead of get

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,24 @@
 Rails.application.routes.draw do
-  get("/",        { :controller => "fortunes", :action => "lucky" })
+  match("/",                { :controller => "fortunes", :action => "lucky",   :via => "get" })
 
-  get("/lucky-numbers",   { :controller => "fortunes", :action => "lucky" })
-  get("/unlucky-numbers", { :controller => "fortunes", :action => "unlucky" })
+  match("/lucky-numbers",   { :controller => "fortunes", :action => "lucky",   :via => "get" })
+  match("/unlucky-numbers", { :controller => "fortunes", :action => "unlucky", :via => "get" })
 
   # PART 1: STATIC ROUTES
   # =====================
 
   # Uncomment each route below ONE AT A TIME and debug.
 
-  # get("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram" })
-  # get("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull" })
-  # get(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins" })
-  # get("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab" })
-  # get ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion" })
-  # get(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden" })
-  # get("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales" })
-  # get("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion" })
-  # get("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer" })
-  # get("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat" })
-  # get("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer" })
-  # get("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish" })
+  # match("/zodiacs/aries",       { :controller =>  zodiacs,  action =>  "ram", :via => "get" })
+  # match("/zodiacs/taurus",      { :controller,   "zodiacs", :action,   "bull", :via => "get" })
+  # match(/zodiacs/gemini,        { :controller => "zodiacs", :action => "twins", :via => "get" })
+  # match("/zodiacs/cancer"       { :controller => "zodiacs"  :action => "crab", :via => "get" })
+  # match ("/zodiacs/leo",        { :controller => "zodiac",  :action => "lion", :via => "get" })
+  # match(" /zodiacs/virgo",      { :controller => "Zodiacs", :action => "maiden", :via => "get" })
+  # match("/zodiacs/libra",       { :controller => "zodiacs", :action => "scales", :via => "get" })
+  # match("/zodiac/scorpio",      { :controller => "zodiacs", :action => "scorpion", :via => "get" })
+  # match("/zodiacs/sagittarius", { :controller => "zodiacs", :action => "archer", :via => "get" })
+  # match("/zodiacs/capricorn",   { :controller => "zodiacs", :action => "goat", :via => "get" })
+  # match("/zodiacs/aquarius",    { :controller => "zodiacs", :action => "waterbearer", :via => "get" })
+  # match("/zodiacs/pisces",      { :controller => "zodiacs", :action => "fish", :via => "get" })
 end
